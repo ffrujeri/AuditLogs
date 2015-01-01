50dc3b9a6569f290f23eb43c2ea36c9f676c3d1d
Felipe Frujeri <ffrujeri@gmail.com>
2015-01-01 15:08:33 +0100

    MerkleTree Hash Update

--- src/MerkleTree.java
+++ src/MerkleTree.java
@@ -1,3 +1,5 @@
+import sun.nio.ByteBuffered;
+
 import java.awt.*;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
@@ -10,39 +12,53 @@ public class MerkleTree {
 	
 	public MerkleTree(String event){
 		hash = getHash(event);
-		// TODO: compute hash;
 	}
 
 	public MerkleTree(MerkleTree left, MerkleTree right){
 		this.left = left;
 		this.right = right;
-		hash = getHash(left.getHash() + right.getHash());
-		// TODO: compute hash (concatenates hashes and hash result)
+		hash = concatenateHash(left.getHash(), right.getHash());
 	}
-	
+
 	private byte[] getHash(String text){
 		try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
 			byte[] h = digest.digest(text.getBytes("UTF-8"));
-			return h.toString(); // TODO: correct
+			return h;
 		}catch(NoSuchAlgorithmException e){
 			System.out.println(e.getMessage());
 			e.printStackTrace();
-		}catch(UnsupportedEncodingException e){
+		}catch(UnsupportedEncodingException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		
 		return null;
 	}
-	
-	public String getHash(){
+
+	private byte[] getHash(byte[] hash){
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] h = digest.digest(hash);
+			return h;
+		}catch(NoSuchAlgorithmException e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}catch(UnsupportedEncodingException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public byte[] getHash(){
 		return hash;
 	}
-	
-	// TODO: tests
-	public static void main(String[] args) {
-		byte[] b = {3, 5, 1, 0};
-		System.out.println(b.toString());
+
+	private static byte[] concatenateHash(byte[] h1, byte[] h2){
+		byte[] hash = new hash[h1.length+h2.length];
+		for (int i = 0; i < h1.length; i++)
+			hash[i]=h1[i];
+		for (int i = 0; i <h2.length ; i++)
+			hash[h1.length+i]=h2[i];
 	}
 }
